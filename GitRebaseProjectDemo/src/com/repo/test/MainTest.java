7347b3bfab30b7aa03d769747696f4e72522d06e
Moukherjee, Sourabrta <sourabrta.moukherjee@mheducation.com>
2018-03-08 22:42:45 +0530

    Master commits

--- GitRebaseProjectDemo/src/com/repo/test/MainTest.java
+++ GitRebaseProjectDemo/src/com/repo/test/MainTest.java
@@ -5,19 +5,8 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");	
-		System.out.println("Master Text");
+		System.out.println("Master Text!");
 
-		System.out.println("Branch1 new Text");
-		System.out.println("Branch New Text");
-		
-		System.out.println("Masterchanges new added");
-		
-
-		System.out.println("New Text in branchone");
-		
-		System.out.println("hi");
-		
-		System.out.println("Hello Master");
 
 }
 
