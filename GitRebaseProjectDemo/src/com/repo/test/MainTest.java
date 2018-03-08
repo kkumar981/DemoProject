fd3edd11c13ceb8d50a93dd133621b451b4abeda
Moukherjee, Sourabrta <sourabrta.moukherjee@mheducation.com>
2018-03-08 18:46:57 +0530

    Hide text in master Branch

--- GitRebaseProjectDemo/src/com/repo/test/MainTest.java
+++ GitRebaseProjectDemo/src/com/repo/test/MainTest.java
@@ -4,11 +4,16 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");	
-		System.out.println("Master branch commits!");	
+		/*System.out.println("Master branch commits!");	
 		System.out.println("Master branch adding1");	
 		System.out.println("Branch commits!!");
 		System.out.println("branch_one new text!!");
 		System.out.println("master brnch new text");
+		
+		System.out.println("Demo Test");
+		System.out.println("new Text Added");*/
+		
+		System.out.println("hiding alal the previous code!!");
 	
 	}
 
