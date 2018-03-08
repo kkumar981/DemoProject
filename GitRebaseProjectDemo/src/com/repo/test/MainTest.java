13e1042f3853498e93eaa1576a6d4a96ceead1da
Rakesh Kumar <kkumar981@gmail.com>
2018-03-08 15:02:50 +0530

    Master files text changes

--- GitRebaseProjectDemo/src/com/repo/test/MainTest.java
+++ GitRebaseProjectDemo/src/com/repo/test/MainTest.java
@@ -3,8 +3,7 @@ package com.repo.test;
 public class MainTest {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-		System.out.println("Branch one text");
+		System.out.println("Hello World");		
 		System.out.println("Master branch commits!");		
 	}
 
