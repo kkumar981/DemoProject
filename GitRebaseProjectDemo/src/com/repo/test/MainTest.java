2c42c92d943cc1a5f5ac7ee0711fe825ffa41350
Moukherjee, Sourabrta <sourabrta.moukherjee@mheducation.com>
2018-03-08 23:26:19 +0530

    commit2

--- GitRebaseProjectDemo/src/com/repo/test/MainTest.java
+++ GitRebaseProjectDemo/src/com/repo/test/MainTest.java
@@ -5,9 +5,7 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");	
-		System.out.println("BranchOne!!");
-			System.out.println("Master!!");
-		System.out.println("Master22");
+	
 }
 
 }
