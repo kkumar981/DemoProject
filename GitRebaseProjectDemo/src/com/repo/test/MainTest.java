a7b172f4d38e89370bb1f0a6cb16606ecb810eda
Rakesh Kumar <kkumar981@gmail.com>
2018-03-08 15:16:04 +0530

    Master text adding

--- GitRebaseProjectDemo/src/com/repo/test/MainTest.java
+++ GitRebaseProjectDemo/src/com/repo/test/MainTest.java
@@ -4,7 +4,8 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");		
-		System.out.println("Master branch commits!");		
+		System.out.println("Master branch commits!");	
+		System.out.println("Master branch adding1");
 	}
 
 }
