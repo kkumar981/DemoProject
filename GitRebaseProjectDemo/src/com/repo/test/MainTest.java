8c0d104fcf3f34624260c7f3fea78716d54ba077
Rakesh Kumar <kkumar981@gmail.com>
2018-03-08 14:45:37 +0530

    Master branch text commits.

--- GitRebaseProjectDemo/src/com/repo/test/MainTest.java
+++ GitRebaseProjectDemo/src/com/repo/test/MainTest.java
@@ -5,6 +5,8 @@ public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Branch one text");
+		System.out.println("Master branch commits!");
+		
 	}
 
 }
