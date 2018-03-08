845ee4cf76b9f8c73d497e3570b51ca3b0bc1dc2
Moukherjee, Sourabrta <sourabrta.moukherjee@mheducation.com>
2018-03-08 20:24:25 +0530

    Master commits in Branch

--- GitRebaseProjectDemo/src/com/repo/test/MainTest.java
+++ GitRebaseProjectDemo/src/com/repo/test/MainTest.java
@@ -5,18 +5,7 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");	
-		/*System.out.println("Master branch commits!");	
-		System.out.println("Master branch adding1");	
-		System.out.println("Branch commits!!");
-		System.out.println("branch_one new text!!");
-		System.out.println("master brnch new text");
-		
-		System.out.println("Demo Test");
-		System.out.println("new Text Added");*/
-		
-		System.out.println("hiding alal the previous code!!");
-		
-		System.out.println("New text in Master branch!!");
+		System.out.println("Master Text");
 	}
 
 }
